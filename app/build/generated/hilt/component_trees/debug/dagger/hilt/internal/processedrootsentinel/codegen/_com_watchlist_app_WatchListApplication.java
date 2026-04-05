989d9c68798e891797e0db1bfbdce87578dafdcf
Marcos <marcoshc3002@gmail.com>
2026-04-05 19:15:47 -0300

    Limpieza de archivos temporales y agregado de .gitignore

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_watchlist_app_WatchListApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.watchlist.app.WatchListApplication"
-)
-public final class _com_watchlist_app_WatchListApplication {
-}
